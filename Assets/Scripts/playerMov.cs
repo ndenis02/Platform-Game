7dc3f090e659f3718502d4cb3aedaca0432fae79
jyip02 <jasonyip28.2001@gmail.com>
2024-02-17 04:37:51 -0500

    2nd commit
    wall jumping but wall sliding still bugging

--- Assets/Scripts/playerMov.cs
+++ Assets/Scripts/playerMov.cs
@@ -5,49 +5,110 @@ using UnityEngine;
 public class playerMov : MonoBehaviour
 {
     //movement values
-    public int walkSpeed;
-    public int sprintSpeed;
-    public int jumpHeight;
-    
-    //Rigidbody reference
+    float h;
+    public float speed;
     Rigidbody2D rb;
 
-    //jump check parameters
+    public float jumpforce;
     public Transform groundCheck;
-    public LayerMask groundMask;
+    public LayerMask groundLayer;
     bool isGrounded;
+    bool jump;
+
+    [Header("Wall Jump System")]
+    public Transform wallCheck;
+    bool isWallTouch;
+    bool isSliding;
+    public float wallSliddingSpeed;
+    public float wallJumpDuration;
+    public Vector2 WallJumpForce;
+    bool WallJumping;
+
+    //Rigidbody reference
+
+    //jump check parameters
 
     //Sounds
     AudioSource jumpSound;
 
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>(); 
+    }
+
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
         jumpSound = GetComponent<AudioSource>();
     }
 
-    void Update()
+    private void Update()
     {
-        Vector2 move = new Vector2(Input.GetAxis("Horizontal"), 0);
-
-        //sprint
-        if (Input.GetKey(KeyCode.LeftShift))
+        h = Input.GetAxisRaw("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            transform.Translate(move * sprintSpeed * Time.deltaTime);
+            jump = true;
         }
-        //walk
+
+        isGrounded = Physics2D.OverlapCapsule(groundCheck.position, new Vector2(1.8f, 0.3f), 0, groundLayer);
+        isWallTouch = Physics2D.OverlapBox(wallCheck.position, new Vector2(0.3f, 1f), 0, groundLayer);
+       
+
+        if (isWallTouch && !isGrounded && h !=0)
+        {
+            isSliding = true;
+        }
         else
         {
-            transform.Translate(move * walkSpeed * Time.deltaTime);
+            isSliding = false;
         }
+        flip();
+    }
 
-        isGrounded = Physics2D.OverlapCapsule(groundCheck.position, new Vector2(1.8f, 0.3f), CapsuleDirection2D.Horizontal, 0, groundMask);
+    private void FixedUpdate()
+    {
 
-        //jump
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (jump)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+            Jump();
             jumpSound.Play();
         }
+        if (isSliding)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSliddingSpeed, float.MaxValue));
+        }
+
+        if (WallJumping)
+        {
+            rb.velocity = new Vector2(-h * WallJumpForce.x, WallJumpForce.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(h * speed, rb.velocity.y);
+        }
+    }
+
+
+    void Jump()
+    {
+        if (isGrounded)
+        {
+            rb.AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
+        }
+        else if (isSliding)
+        {
+            WallJumping = true;
+            Invoke("StopWallJumping", wallJumpDuration);
+        }
+        jump = false;
+    }
+    void StopWallJumping()
+    {
+        WallJumping = false;
+    }
+
+    void flip()
+    {
+        if (h < -0.01f) transform.localScale = new Vector3(-1, 1, 1);
+        if (h > 0.01f) transform.localScale = new Vector3(1, 1, 1);
     }
 }
