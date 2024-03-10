7bfa7aa66c660e5307b02b4836ec7a27b94d8614
ndenis02 <101900282+ndenis02@users.noreply.github.com>
2024-03-10 01:15:26 -0500

    Update HeadDetectScript.cs

--- Assets/Scripts/HeadDetectScript.cs
+++ Assets/Scripts/HeadDetectScript.cs
@@ -5,19 +5,24 @@ using UnityEngine;
 public class HeadDetectScript : MonoBehaviour
 {
     GameObject Enemy;
+    public string targetTag = "Player"; // Specify the tag you want to detect collisions with
 
     // Start is called before the first frame update
     void Start()
     {
         Enemy = gameObject.transform.parent.gameObject;
     }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("Collision with: " + collision.gameObject.name);
-        GetComponent<Collider2D>().enabled = false;
-        Enemy.GetComponent<SpriteRenderer>().flipY = true;
-        Enemy.GetComponent<Collider2D>().enabled = false;
-        Vector3 movement = new Vector3(Random.Range(40, 70), Random.Range(-40, 40), 0f);
-        Enemy.transform.position += movement * Time.deltaTime;
+        if (collision.gameObject.CompareTag(targetTag)) // Check if the collided object has the specified tag
+        {
+            Debug.Log("Collision with: " + collision.gameObject.name);
+            GetComponent<Collider2D>().enabled = false;
+            Enemy.GetComponent<SpriteRenderer>().flipY = true;
+            Enemy.GetComponent<Collider2D>().enabled = false;
+            Vector3 movement = new Vector3(Random.Range(40, 70), Random.Range(-40, 40), 0f);
+            Enemy.transform.position += movement * Time.deltaTime;
+        }
     }
 }
